b6f52473a2ecdb28bfe099cd29a58edb30095dfd
PetrPoling <46245539+PetrPoling@users.noreply.github.com>
2023-03-25 00:23:58 +0100

    1

--- Start Prm/Program.cs
+++ Start Prm/Program.cs
@@ -4,54 +4,60 @@ using TaskStatus = System.Threading.Tasks.TaskStatus;
 
 namespace Project_Manager
 {
-    class Program
-    {
+    public class Program
+    {string project = "try" ;
+
         static void Main(string[] args)
-        {
+        {   
             string statement = "1";
             while (statement == "1")
-            {  
-                Console.WriteLine("Please enter project name");
-                string projectName = Console.ReadLine();
-                Console.WriteLine("Please enter project description");
-                string projectDescription = Console.ReadLine();
-                Console.WriteLine("Please enter project start date in format \"yyyy-MM-dd\"");
-                DateTime projectStartDate = DateTime.Parse(Console.ReadLine());
-                Console.WriteLine("Please enter project end date in format \"yyyy-MM-dd\"");
-                DateTime projectEndDate = DateTime.Parse(Console.ReadLine());
-                Console.WriteLine("Please enter project budget");
-                double projectBudget = double.Parse(Console.ReadLine());
-                Console.WriteLine("Please enter task name");
-                string taskName = Console.ReadLine();
-                Console.WriteLine("Please enter task description");
-                string taskDescription = Console.ReadLine();
-                Console.WriteLine("Please enter task start date in format \"yyyy-MM-dd\"");
-                DateTime taskStartDate = DateTime.Parse(Console.ReadLine());
-                Console.WriteLine("Please enter task end date in format \"yyyy-MM-dd\"");
-                DateTime taskEndDate = DateTime.Parse(Console.ReadLine());
-                Console.WriteLine("Please enter task budget");
-                double taskBudget = double.Parse(Console.ReadLine());
-                Console.WriteLine("Please enter team member name");
-                string teamMemberName = Console.ReadLine();
-                Console.WriteLine("Please enter team member email");
-                string teamMemberEmail = Console.ReadLine();
+            {
+                {
+                    Console.WriteLine("Please enter project name");
+                    string projectName = Console.ReadLine();
+                    Console.WriteLine("Please enter project description");
+                    string projectDescription = Console.ReadLine();
+                    Console.WriteLine("Please enter project start date in format \"yyyy-MM-dd\"");
+                    DateTime projectStartDate = DateTime.Parse(Console.ReadLine());
+                    Console.WriteLine("Please enter project end date in format \"yyyy-MM-dd\"");
+                    DateTime projectEndDate = DateTime.Parse(Console.ReadLine());
+                    Console.WriteLine("Please enter project budget");
+                    double projectBudget = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Please enter task name");
+                    string taskName = Console.ReadLine();
+                    Console.WriteLine("Please enter task description");
+                    string taskDescription = Console.ReadLine();
+                    Console.WriteLine("Please enter task start date in format \"yyyy-MM-dd\"");
+                    DateTime taskStartDate = DateTime.Parse(Console.ReadLine());
+                    Console.WriteLine("Please enter task end date in format \"yyyy-MM-dd\"");
+                    DateTime taskEndDate = DateTime.Parse(Console.ReadLine());
+                    Console.WriteLine("Please enter task budget");
+                    double taskBudget = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Please enter team member name");
+                    string teamMemberName = Console.ReadLine();
+                    Console.WriteLine("Please enter team member email");
+                    string teamMemberEmail = Console.ReadLine();
 
-                Project project = new Project();
-                project.CreateProject(projectName, projectDescription, projectStartDate, projectEndDate, projectBudget);
+                    Project project = new Project();
+                    project.CreateProject(projectName, projectDescription, projectStartDate, projectEndDate,
+                        projectBudget);
 
-                Task task1 = new Task();
-                task1.CreateTask(taskName, taskDescription, taskStartDate, taskEndDate, taskBudget);
-                project.AddTask(task1);
+                    Task task1 = new Task();
+                    task1.CreateTask(taskName, taskDescription, taskStartDate, taskEndDate, taskBudget);
+                    project.AddTask(task1);
 
-                TeamMember teamMember1 = new TeamMember();
-                teamMember1.CreateTeamMember(teamMemberName, teamMemberEmail);
-                task1.AddTeamMember(teamMember1);
-                project.GenerateReport();
-                Console.WriteLine("If you want to exit pres 0 if not press 1");
-                statement = Console.ReadLine();
-                
+                    TeamMember teamMember1 = new TeamMember();
+                    teamMember1.CreateTeamMember(teamMemberName, teamMemberEmail);
+                    task1.AddTeamMember(teamMember1);
+                    Console.WriteLine("If you want to exit pres 0 if not press 1");
+                    statement = Console.ReadLine();
+                    project.GenerateReport();
+                    
+                }
             }
+            
         }
+
     }
     public class Project
     {
